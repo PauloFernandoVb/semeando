76e72e639e933ea1c3ec01bd8f6fcdaede00987e
PauloFernandoVb <168550782+PauloFernandoVb@users.noreply.github.com>
2025-10-23 18:26:45 -0300

    Atualizar o usuarioRoute.js

--- routes/usuarioRoute.js
+++ routes/usuarioRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const UsuarioController = require('../controllers/usuarioController');
 
+let router = express.Router();
+
 let ctrl = new UsuarioController();
 
-let router = express.Router();
 
 router.get('/cadastrar',ctrl.cadastroView);
 router.post('/cadastrar',ctrl.cadastrar);
